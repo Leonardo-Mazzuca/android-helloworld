a9ed3c81d692cd11283ffc85867a786b4746e526
Leonardo-Mazzuca <leonardodesouzamazzuca8@gmail.com>
2025-03-06 12:07:20 -0300

    Retirando imports inúteis e melhorando comentários

--- app/src/main/java/leonardomazzuca/com/github/helloworld/MainActivity.kt
+++ app/src/main/java/leonardomazzuca/com/github/helloworld/MainActivity.kt
@@ -1,23 +1,59 @@
 package leonardomazzuca.com.github.helloworld
 
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import leonardomazzuca.com.github.helloworld.ui.theme.HelloWorldTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
+    /*
+           Podemos dizer que isso se equipara ao document.getElementById do JS
+           Dentro dos <> passar o tipo de elemento
+           O R é a variavél que acessa o layout
+          Caso já tenhamos algum elemento com o id, o auto-complete será realizado
+     */
+
+
+        val inputName = findViewById<EditText>(R.id.etName);
+        val message = findViewById<TextView>(R.id.textViewHello);
+
+//        Podemos pensar que isso seria igual a um listener de input do JS
+        inputName.setOnEditorActionListener{textView, actionId, _ ->
+
+            if(actionId == EditorInfo.IME_ACTION_DONE) {
+
+                //Limpa espaços em branco com método trim()
+                val name = textView.text.toString().trim()
+
+                //if inline aqui para escrever menos código, parecido com operador ternário
+                message.text = if (name.isNotEmpty()) "Hello world! $name" else "Hello world!"
+
+                /*
+                    Mesma coisa que
+
+                    if (name.isNotEmpty()) {
+                        message.text = "Hello world! $name"
+                    } else {
+                        message.text = "Hello world!"
+                    }
+                */
+
+
+                //indicação de que a ação foi tratada no listener
+                true
+            } else {
+                //indicação de que a ação não foi tratada no listener
+                false
+            }
+
+        }
     }
 
 
